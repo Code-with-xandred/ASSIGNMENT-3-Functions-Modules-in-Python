9e3e8f8cf5bd5884630044377f0255c48006ab67
Code-with-xandred <stak55795@gmail.com>
2025-02-28 00:10:14 +0530

    Task 2

---
+++ Calculations.py
@@ -0,0 +1,13 @@
+# Task 2: Using the Math Module for Calculations
+
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+natural_log = math.log(number)  
+sine = math.sin(number)  
+
+print(f"Square root: {square_root}")
+print(f"Logarithm: {natural_log}")
+print(f"Sine: {sine}")
